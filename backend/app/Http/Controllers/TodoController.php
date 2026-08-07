efcb99c4157731a2158d99ed7b97eca4a9c60528
Dewni Jayasuriya <127865794+dewnijayasuriya@users.noreply.github.com>
2026-08-07 20:52:30 +0530

    feate(todo):Implement authenticated create todo API

--- backend/app/Http/Controllers/TodoController.php
+++ backend/app/Http/Controllers/TodoController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    //
+    public function store(Request $request)
+{
+    $validated = $request->validate([
+        'title' => 'required|string|max:255', // Ensures title is required, must be a string and should not exceed 255 characters
+        'description' => 'nullable|string', // Description is optional but must be text if provided.
+    ]);
+
+    $todo = Todo::create([ // inserts a new row into PostgreSQL.
+        'title' => $validated['title'],
+        'description' => $validated['description'] ?? null, // If description exists use it
+        'completed' => false, // Every new Todo starts as incomplete.
+        'user_id' => auth()->id(), // gets the ID of the currently authenticated user from the Sanctum token.
+    ]);
+
+    return response()->json([
+        'message' => 'Todo created successfully',
+        'todo' => $todo,
+    ], 201);
+}
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 
 // Public Routes
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,6 +11,12 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/todos', [TodoController::class, 'store']);
+
+});
 
 // Protected Route (default)
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
